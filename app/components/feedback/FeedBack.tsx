d77253e1bb30fc39659e4e34a39e605e07601413
Kieu Quang Vinh <vinhkqph25485@fpt.edu.vn>
2024-08-19 08:40:24 +0700

    update

--- app/components/feedback/FeedBack.tsx
+++ app/components/feedback/FeedBack.tsx
@@ -81,98 +81,102 @@ const Feedback = () => {
 
   return (
     <>
-      <div className="mx-5 bg-black text-white py-10">
-        <div className="text-white mb-5 mx-5 opacity-70">● KHÁCH HÀNG NÓI GÌ VỀ CHÚNG TÔI</div>
-        <div className={style.wrapper} onClick={handleClick}>
-          <div
-            style={{
-              margin: "35px 0 55px 0",
-              width: "100%",
-              height: "2px",
-              backgroundColor: "#000000",
-              position: "relative",
-              borderRadius: "10px",
-              overflow: "hidden",
-            }}
-          >
-            <div
-              style={{
-                width: `${progress}%`,
-                height: "100%",
-                backgroundColor: "#ffffff",
-                transition: "width 0.5s ease",
-              }}
-            ></div>
+      <div className="1920px">
+        <div className="mx-5 bg-black text-white py-10">
+          <div className="text-white mb-5 mx-5 opacity-70">
+            ● KHÁCH HÀNG NÓI GÌ VỀ CHÚNG TÔI
           </div>
-          <div className="flex justify-between w-100 h-100">
-            <div className="flex items-start gap-[16px]">
-              <span className="flex items-center relative w-[80px]">
-                <Image
-                  src="/Ellipse 40.png"
-                  alt="feedback"
-                  width={37}
-                  height={37}
-                />
-                <Image
-                  src="/Ellipse 41.png"
-                  alt="feedback"
-                  width={37}
-                  height={37}
-                  style={{ position: "absolute", left: "20px" }}
-                />
-                <Image
-                  src="/Ellipse 42.png"
-                  alt="feedback"
-                  width={37}
-                  height={37}
-                  style={{ position: "absolute", left: "40px" }}
-                />
-                <Image
-                  src="/Ellipse 43.png"
-                  alt="feedback"
-                  width={37}
-                  height={37}
-                  style={{ position: "absolute", left: "60px" }}
-                />
-              </span>
-              <div className="flex gap-3 mt-1 ml-5">
-                <span style={{ opacity: "0.5" }}>
-                  {feedbacks[currentFeedback].collaborations}
-                </span>
-                <span style={{ opacity: "0.5" }}>
-                  {feedbacks[currentFeedback].projects}
-                </span>
-              </div>
-            </div>
+          <div className={style.wrapper} onClick={handleClick}>
             <div
-              className={`flex flex-col gap-[52px] ${style.feedbackItem}`}
               style={{
-                maxWidth: "666px",
-                animationName: animating ? getNewAnimationClass() : "",
+                margin: "35px 0 55px 0",
+                width: "100%",
+                height: "2px",
+                backgroundColor: "#000000",
+                position: "relative",
+                borderRadius: "10px",
+                overflow: "hidden",
               }}
             >
-              <span style={{ maxWidth: "666px", fontSize: "28px" }}>
-                {feedbacks[currentFeedback].quote}
-              </span>
-              <span
+              <div
                 style={{
-                  fontSize: "16px",
-                  display: "inline-flex",
-                  gap: "10px",
+                  width: `${progress}%`,
+                  height: "100%",
+                  backgroundColor: "#ffffff",
+                  transition: "width 0.5s ease",
+                }}
+              ></div>
+            </div>
+            <div className="flex justify-between w-100 h-100">
+              <div className="flex items-start gap-[16px]">
+                <span className="flex items-center relative w-[80px]">
+                  <Image
+                    src="/Ellipse 40.png"
+                    alt="feedback"
+                    width={37}
+                    height={37}
+                  />
+                  <Image
+                    src="/Ellipse 41.png"
+                    alt="feedback"
+                    width={37}
+                    height={37}
+                    style={{ position: "absolute", left: "20px" }}
+                  />
+                  <Image
+                    src="/Ellipse 42.png"
+                    alt="feedback"
+                    width={37}
+                    height={37}
+                    style={{ position: "absolute", left: "40px" }}
+                  />
+                  <Image
+                    src="/Ellipse 43.png"
+                    alt="feedback"
+                    width={37}
+                    height={37}
+                    style={{ position: "absolute", left: "60px" }}
+                  />
+                </span>
+                <div className="flex gap-3 mt-1 ml-5">
+                  <span style={{ opacity: "0.5" }}>
+                    {feedbacks[currentFeedback].collaborations}
+                  </span>
+                  <span style={{ opacity: "0.5" }}>
+                    {feedbacks[currentFeedback].projects}
+                  </span>
+                </div>
+              </div>
+              <div
+                className={`flex flex-col gap-[52px] ${style.feedbackItem}`}
+                style={{
+                  maxWidth: "666px",
+                  animationName: animating ? getNewAnimationClass() : "",
                 }}
               >
-                <p>{feedbacks[currentFeedback].author}</p>
-                <p style={{ opacity: 0.4 }}>
-                  {" "}
-                  - {feedbacks[currentFeedback].position}
-                </p>
-              </span>
-            </div>
-            <div className="mt-[-20px]" style={{ fontSize: "50px" }}>
-              <span>{String(currentFeedback + 1).padStart(2, "0")}</span>
-              <span style={{ opacity: "0.2" }}>
-                /{String(feedbacks.length).padStart(2, "0")}
-              </span>
+                <span style={{ maxWidth: "666px", fontSize: "28px" }}>
+                  {feedbacks[currentFeedback].quote}
+                </span>
+                <span
+                  style={{
+                    fontSize: "16px",
+                    display: "inline-flex",
+                    gap: "10px",
+                  }}
+                >
+                  <p>{feedbacks[currentFeedback].author}</p>
+                  <p style={{ opacity: 0.4 }}>
+                    {" "}
+                    - {feedbacks[currentFeedback].position}
+                  </p>
+                </span>
+              </div>
+              <div className="mt-[-20px]" style={{ fontSize: "50px" }}>
+                <span>{String(currentFeedback + 1).padStart(2, "0")}</span>
+                <span style={{ opacity: "0.2" }}>
+                  /{String(feedbacks.length).padStart(2, "0")}
+                </span>
+              </div>
             </div>
           </div>
         </div>
